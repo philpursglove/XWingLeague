ae0ef83b2ae13acd69cb0bf9130e4f3212c1a258
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-01-02 13:26:22 +0000

    Witing up automatic database updates

--- XWingLeague/XWingLeague/Global.asax.cs
+++ XWingLeague/XWingLeague/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Configuration;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using XWingLeague.DatabaseMigrations;
 
 namespace XWingLeague
 {
@@ -16,6 +14,16 @@ namespace XWingLeague
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            InitialiseDatabase();
         }
+
+        private static void InitialiseDatabase()
+        {
+            var connectionStringSettings = ConfigurationManager.ConnectionStrings["XWingLeague"];
+            var databaseMigrator = new Migrator(connectionStringSettings.ConnectionString);
+            databaseMigrator.MigrateToLatestSchema();
+        }
+
     }
 }
