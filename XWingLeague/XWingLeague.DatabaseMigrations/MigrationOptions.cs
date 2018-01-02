ff4a73d8384390c9232f40ebc899b741454f9bca
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-01-02 13:26:48 +0000

    Infrastructre for db migrations

---
+++ XWingLeague/XWingLeague.DatabaseMigrations/MigrationOptions.cs
@@ -0,0 +1,11 @@
+﻿using FluentMigrator;
+
+namespace XWingLeague.DatabaseMigrations
+{
+    public class MigrationOptions : IMigrationProcessorOptions
+    {
+        public bool PreviewOnly { get; set; }
+        public int Timeout { get; set; }
+        public string ProviderSwitches { get; set; }
+    }
+}
---
+++ XWingLeague/XWingLeague.DatabaseMigrations/Migrator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors.SqlServer;
+
+namespace XWingLeague.DatabaseMigrations
+{
+    public class Migrator
+    {
+        private readonly string connectionString;
+
+        public Migrator(string connectionString)
+        {
+            if (connectionString == null)
+            {
+                throw new ArgumentNullException("connectionString");
+            }
+
+            this.connectionString = connectionString;
+        }
+
+        public void MigrateToLatestSchema()
+        {
+            var todaysDate = DateTime.Today.ToString("yyyyMMdd");
+            var todaysSchemaVersion = long.Parse(todaysDate);
+            MigrateTo(todaysSchemaVersion);
+        }
+
+        private void MigrateTo(long targetVersion)
+        {
+            var options = new MigrationOptions { PreviewOnly = false, Timeout = 60 };
+            var announcer = new NullAnnouncer();
+            var processor = new SqlServer2012ProcessorFactory().Create(connectionString, announcer, options);
+            var migrationContext = new RunnerContext(announcer) { Namespace = "XWingLeague.DatabaseMigrations.Migrations" };
+
+            var runner = new MigrationRunner(GetType().Assembly, migrationContext, processor);
+            runner.MigrateUp(targetVersion, true);
+        }
+
+    }
+}
