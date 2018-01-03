05b8aaad22943462cf00927d8949fa5e101f68a2
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-01-03 16:55:54 +0000

    League model

---
+++ XWingLeague/XWingLeague/Models/League.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XWingLeague.Models
+{
+    public class League
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public List<ApplicationUser> Administrators { get; set; }
+        public List<ApplicationUser> Players { get; set; }
+    }
+}
